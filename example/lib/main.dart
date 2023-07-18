258e39b610d8467f48da4ef34e13f6cc190f5cd5
Shivam Mishra <shivbo.mishra96@gmail.com>
2023-07-18 14:42:10 +0530

    * fixed minor issue with animated cursor ** added `prefilled color property` in OtpPinFieldStyle *** documentation updated

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -79,10 +79,10 @@ class _MyHomePageState extends State<MyHomePage> {
                 activeFieldBackgroundColor: Colors.cyanAccent,
 
                 /// Background Color for filled field pin box
-                filledFieldBackgroundColor: Colors.transparent,
+                filledFieldBackgroundColor: Colors.green,
 
                 /// border Color for filled field pin box
-                filledFieldBorderColor: Colors.transparent,
+                filledFieldBorderColor: Colors.green,
               ),
               maxLength: 4,
 
